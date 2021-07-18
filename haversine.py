e8718f5b2ab8e47ea1abd91008b256f809be55a2
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-18 16:22:21 -0500

    added cong. session dates and haversine function

---
+++ haversine.py
@@ -0,0 +1,28 @@
+'''
+Calculates the distance between two coordinate points
+'''
+
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+lat1 = 52.2296756
+lat2 = 52.406374
+lon1 = 21.0122287
+lon2 = 16.9251681
+
+print(haversine(lon1, lat1, lon2, lat2))
