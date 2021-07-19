f4d28199a68e5debf7d06e1bc67c4aca792ab192
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-18 20:50:07 -0500

    added code to retreive the number of coordinates for each polygon

--- polygon_areas.py
+++ polygon_areas.py
@@ -13,9 +13,9 @@ with open('/Users/curtissmith/Projects/gerrymandering_large_files/congressional-
 
 # Step 1: Get Number of Districts
 num_districts = len(data['features'])
-print(num_districts)
+print(f'Number of Districts: {num_districts}')
+
 districts = list(range(num_districts))
-print(districts)
 
 # Step 2: initialize list of dictionaries to hold data
 info_list = []
@@ -25,8 +25,20 @@ for i in districts:
     district_num = data['features'][i]['properties']['district']
     # appends district number to list of dictionaries
     num_polygons = len(data['features'][i]['geometry']['coordinates'])
-    info_list.append({"district":district_num, "polygon_count": num_polygons})
+    # district count is place in the "districts" list; district is the actual district number
+    # the district number can vary; some will be "0"
+    info_list.append({"district_count": i, "district":district_num, "polygon_count": num_polygons})
 
-print(info_list)
+# print(info_list)
 
 # Step 4: get number of coordinates for each polygon; could be intermediate step, might delete later
+for i in districts:
+    # print(i)
+    num_polygons = info_list[i]['polygon_count']
+    for x in range(num_polygons):
+        coord_count = len(data['features'][i]['geometry']['coordinates'][x][0])
+        key_string = f'polygon{x+1}_count'
+        info_list[i][key_string] = coord_count
+print(info_list)
+
+# Step 5: Calculate polygon area
