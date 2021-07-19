b711fba075bbdcff90d17b06484607a3a1ad576b
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-19 10:59:39 -0500

    added JSON structure for holding all data, completed/simplified function to calculate area/perimeter/pp score

--- polygon_areas.py
+++ polygon_areas.py
@@ -1,37 +1,11 @@
 import json
+import os
 import math
-from logging import info
-import pyproj    
 from pyproj import Geod
-import shapely
-import shapely.ops as ops
-from shapely.geometry.polygon import Polygon
-from functools import partial
-import warnings
 
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
 
 # Defining functions
-
-def area_calculator(list_of_coords):
-    '''
-    Calculates the area from an input list of coordinate tuples
-    '''
-    geom = Polygon(list_of_coords)
-    geom_area = ops.transform(
-        partial(
-            pyproj.transform,
-            pyproj.Proj(init='EPSG:4326'),
-            pyproj.Proj(
-                proj='aea',
-                lat_1=geom.bounds[1],
-                lat_2=geom.bounds[3]
-            )
-        ),
-        geom)
-    area_m2 = geom_area.area
-    area_mi2 = area_m2 * 0.00000038610
-    return area_mi2
 
 # area + perim function
 def area_perimeter(lats, longs):
@@ -46,75 +20,82 @@ def pp_test(area, perimeter):
     return pp_score
 
 
-with open('/Users/curtissmith/Projects/gerrymandering_large_files/congressional-district-boundaries-master/Alabama_108_to_112.geojson') as test:
-    data = json.loads(test.read())
+# initialize main_dictionary, where all calculated informtion for each district will be stored
+main_dictionary =  {"entries": []}
 
-# coordinates = data['features'][0]['geometry']['coordinates'][0][0]
-# print(len(coordinates))
-# # print(coordinates)
+file = '/Users/curtissmith/Projects/gerrymandering_large_files/congressional-district-boundaries-master/Alabama_108_to_112.geojson'
 
-# properties = data['features'][0]['properties']['district']
-# print(properties)
+def info_gatherer(file):
+    # Step 0: Open file
+    with open(file) as test:
+        data = json.loads(test.read())
 
-# Step 1: Get Number of Districts
-num_districts = len(data['features'])
-print(f'Number of Districts: {num_districts}')
+    # Step 1: Get Number of Districts
+    num_districts = len(data['features'])
+    # print(f'Number of Districts: {num_districts}')
 
-districts = list(range(num_districts))
+    districts = list(range(num_districts))
 
-# Step 2: initialize list of dictionaries to hold data
-info_list = []
+    # Step 2: initialize list of dictionaries to hold data
+    info_list = []
 
-# Step 3: Get District Numbers and polygon count
-for i in districts:
-    district_num = data['features'][i]['properties']['district']
-    # appends district number to list of dictionaries
-    num_polygons = len(data['features'][i]['geometry']['coordinates'])
-    # district count is place in the "districts" list; district is the actual district number
-    # the district number can vary; some will be "0"
-    info_list.append({"district_count": i, "district":district_num, "polygon_count": num_polygons})
+    # Step 3: Get District Numbers and polygon count
+    # Iterate through list of districts
+    for i in districts:
+        district_num = data['features'][i]['properties']['district']
+        # appends district number to list of dictionaries
+        num_polygons = len(data['features'][i]['geometry']['coordinates'])
+        # district count is place in the "districts" list; district is the actual district number
+        # the district number can vary; some will be "0"
+        info_list.append(
+            {"district_count": i, 
+            "district": district_num, 
+            "polygon_count": num_polygons,
+            "polygon_list":[]
+            })
 
-# print(info_list)
+        # Step 4: Iterate through polygons
+        num_polygons = info_list[i]['polygon_count']
+        for polygon in range(num_polygons):
+            coord_count = len(data['features'][i]['geometry']['coordinates'][polygon][0])
+            # Step 5: Calculate polygon area and perimeter
+            # Get list of coordinates for each polygon
+            coord_list = data['features'][i]['geometry']['coordinates'][polygon][0]
 
-# Step 4: get number of coordinates for each polygon; could be intermediate step, might delete later
-for i in districts:
-    # print(i)
-    num_polygons = info_list[i]['polygon_count']
-    for x in range(num_polygons):
-        coord_count = len(data['features'][i]['geometry']['coordinates'][x][0])
-        key_string = f'polygon{x+1}_count'
-        info_list[i][key_string] = coord_count
-        # Step 5: Calculate polygon area
-        # Get list of coordinates for each polygon
-        coord_list = data['features'][i]['geometry']['coordinates'][x][0]
+            # Returns list of lats and list of longs
+            lats = []
+            longs = []
+            for coord in coord_list:
+                longs.append(coord[0])
+                lats.append(coord[1])
+            area, perimeter = area_perimeter(lats, longs)
 
-        # Returns list of lats and list of longs
-        lats = []
-        longs = []
-        for coord in coord_list:
-            longs.append(coord[0])
-            lats.append(coord[1])
-        area, perimeter = area_perimeter(lats, longs)
+            # Calculate the Polsby-Popper Score
+            pp_score = pp_test(area, perimeter)
 
-        # Calculate the Polsby-Popper Score
-        pp_score = pp_test(area, perimeter)
+            # Add info to list
+            # Located in 'polygon_list':[{}, {}]
+            info_list[i]["polygon_list"].append({
+                "polygon_num": polygon,
+                "polygon_count": coord_count,
+                "polygon_area": area, 
+                "polygon_perimeter": perimeter,
+                "pp_score": pp_score, 
+            })
 
-        # Add info to list
-        key_string = f'polygon{x+1}_area'
-        info_list[i][key_string] = area
-        key_string = f'polygon{x+1}_perimeter'
-        info_list[i][key_string] = perimeter
-        key_string = f'polygon{x+1}_pp_score'
-        info_list[i][key_string] = pp_score
+    # print(info_list)
 
+    # Create export dictionary
+    export_dict = {"geo_info":info_list}
+    export_dict["properties"] = data["features"][0]["properties"]
+    # print(export_dict)
+    return export_dict
 
-# Step 6: Calculate Perimeter
+export_dict = info_gatherer(file)
 
-# print(info_list)
+# Add Export dictionary to main dictionary
+main_dictionary["entries"].append(export_dict)
+print(main_dictionary)
 
-# Step X: Create export dictionary
-export_dict = {"geo_info":info_list}
-export_dict["properties"] = data['features'][0]['properties']
-print(export_dict)
 
 
