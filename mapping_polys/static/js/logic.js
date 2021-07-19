91256f1bf6170e6e514251db24141a956d4c2fbd
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-18 19:43:08 -0500

    added js file to visualize areas prior to calculations

---
+++ mapping_polys/static/js/logic.js
@@ -0,0 +1,55 @@
+// // Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite Streets": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let congDistrict = "https://raw.githubusercontent.com/JeffreyBLewis/congressional-district-boundaries/master/Alabama_16_to_17.geojson";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "blue",
+    fillColor: "yellow",
+    weight: 1
+}
+
+// Grabbing our GeoJSON data.
+d3.json(congDistrict).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, { 
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h2>" + "District: " + feature.properties.district + "</h2>");
+        }
+    }).addTo(map);
+});
