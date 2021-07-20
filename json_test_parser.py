593e97e947b29bf55b912d2d68c384ba328b5f0e
curtissmith291 <82423123+curtissmith291@users.noreply.github.com>
2021-07-20 14:34:38 -0500

    cleaned up code

--- json_test_parser.py
+++ json_test_parser.py
@@ -16,7 +16,9 @@ def area_perimeter(lats, longs):
     return abs(poly_area), poly_perimeter
 
 def pp_test(area, perimeter):
-    if area == 0 and perimeter == 0:
+    if area == -0 or perimeter == -0:
+        pp_score = "error: area or perimeter = -0"
+    elif area == 0 and perimeter == 0:
         pp_score = "error: area & perimeter = 0"
     else:
         pp_score = (4 * math.pi * area) / (perimeter ** 2)
@@ -29,7 +31,7 @@ def info_gatherer(file):
 
     # Step 1: Get Number of Districts
     num_districts = len(data['features'])
-    print(f'Number of Districts: {num_districts}')
+    # print(f'Number of Districts: {num_districts}')
 
     districts = list(range(num_districts))
     # print(districts)
@@ -43,7 +45,7 @@ def info_gatherer(file):
     # Step 3: Get District Numbers and polygon count
     # Iterate through list of districts
     for i in districts:
-        print(f'i: {i}')
+        # print(f'i: {i}')
         district_num = data['features'][i]['properties']['district']
         # appends district number to list of dictionaries
         num_polygons = len(data['features'][i]['geometry']['coordinates'])
@@ -61,19 +63,17 @@ def info_gatherer(file):
         # Step 4: Iterate through polygons
         num_polygons = info_list[i]['polygon_count']
         for polygon in range(num_polygons):
-            print(f'polygon: {polygon} of {num_polygons}')
+            # print(f'polygon: {polygon} of {num_polygons}')
             num_coordinates = len(data['features'][i]['geometry']['coordinates'][polygon])
-            print(f'Number of Coordinates: {num_coordinates}')
-            error = False
-            error_message = ''
+            # print(f'Number of Coordinates: {num_coordinates}')
             if num_coordinates == 1:
                 # print("path 1")
                 coord_count = len(data['features'][i]['geometry']['coordinates'][polygon][0])
-                print(f'Coordinat Count: {coord_count}')
+                # print(f'Coordinat Count: {coord_count}')
                 # Step 5: Calculate polygon area and perimeter
                 # Get list of coordinates for each polygon
                 coord_list = data['features'][i]['geometry']['coordinates'][polygon][0]
-                print(coord_list)
+                # print(coord_list)
                 # if len(coord_list) < 3:
                 #     # print(len(coord_list))
                 #     # print(coord_list)
@@ -89,29 +89,26 @@ def info_gatherer(file):
                 # print(lats)
                 # print(longs)
                 area, perimeter = area_perimeter(lats, longs)
-                print(f'perimeter: {perimeter}')
-                print(f'area: {area}')
+                # print(f'perimeter: {perimeter}')
+                # print(f'area: {area}')
                 # Some jsons contain lists of the same values of corrdinates; causes "0" or "-0" for perimiter
                 # 0 for perimiter will be flagged as erronious data
-                if perimeter == 0 or perimeter == -0:
-                    # "-0" breaks pp_test, convert to "0"
-                    perimeter = abs(perimeter)
-                    print(f'perimeter: {perimeter}')
-                    error = True
-                    message = "0 perimeter; repeating coordinates"
+                # if perimeter == 0 or perimeter == -0:
+                #     # "-0" breaks pp_test, convert to "0"
+                #     perimeter = abs(perimeter)
+                #     print(f'perimeter: {perimeter}')
+                #     error = True
+                #     message = "0 perimeter; repeating coordinates"
                 # Calculate the Polsby-Popper Score
                 pp_score = pp_test(area, perimeter)
-                print(f'pp_score: {pp_score}')
+                # print(f'pp_score: {pp_score}')
                 # Add info to list
                 # Located in 'polygon_list':[{}, {}]
-                if error == True:
-                    error_message = message
                 info_list[i]["polygon_list"].append({
                     "polygon_num": polygon,
                     "polygon_count": coord_count,
                     "polygon_area": area, 
                     "polygon_perimeter": perimeter,
-                    "error": error_message,
                     "pp_score": pp_score, 
                 })
                 # print(info_list)
@@ -176,7 +173,7 @@ def info_gatherer(file):
 
     # Create export dictionary
     export_dict = {"geo_info":info_list, "properties":properties_list}
-    print(f'Export dict {export_dict}')
+    # print(f'Export dict {export_dict}')
     return export_dict
 
 # --------------------
@@ -192,34 +189,34 @@ path = '/Users/curtissmith/Projects/gerrymandering_large_files/congressional-dis
 # filename = 'Oklahoma_60_to_62.geojson'
 # filename = 'Alabama_108_to_112.geojson'
 # filename = 'South_Carolina_93_to_97.geojson'
-filename = 'New_York_94_to_97.geojson'
+# filename = 'New_York_94_to_97.geojson'
 
 
-file = os.path.join(path, filename)
+# file = os.path.join(path, filename)
 
-try:
-    data_dict = info_gatherer(file)
+# try:
+#     data_dict = info_gatherer(file)
 
-    # Append export dictionary to main dictionary
-    main_dictionary["entries"].append(data_dict)
-except:
-    error_list.append(filename)
+#     # Append export dictionary to main dictionary
+#     main_dictionary["entries"].append(data_dict)
+# except:
+#     error_list.append(filename)
 
-# print("running")
-# for filename in os.listdir(path):
-#     file = os.path.join(path, filename)
+print("running")
+for filename in os.listdir(path):
+    file = os.path.join(path, filename)
 
-#     try:
-#         data_dict = info_gatherer(file)
+    try:
+        data_dict = info_gatherer(file)
 
-#         # Append export dictionary to main dictionary
-#         main_dictionary["entries"].append(data_dict)
-#     except:
-#         error_list.append(filename)
+        # Append export dictionary to main dictionary
+        main_dictionary["entries"].append(data_dict)
+    except:
+        error_list.append(filename)
 
-# # Export data
-# with open("gerrymandering_large_files/output.json", 'w') as out_file:
-#     json.dump(main_dictionary, out_file, indent = 4)
+# Export data
+with open("gerrymandering_large_files/output.json", 'w') as out_file:
+    json.dump(main_dictionary, out_file, indent = 4)
 
 print("Complete")
 print(f'Errors: {error_list}')
